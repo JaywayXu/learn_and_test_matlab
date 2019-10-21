7bb13778aee68577cd453dd4971e5c6bf9bab558
neo_cloud <1786546913@qq.com>
2019-10-21 19:44:37 +0800

    update

---
+++ Operations_operators/test_all_1.m
@@ -0,0 +1,9 @@
+A=[0 1 2;3 4 5]
+disp('all(A)')
+all(A)
+disp('all(all(A))')
+all(all(A)) % 按照列进行操作
+disp('A>=1')
+A>=1
+disp('all(A>=1)')
+all(A>=1)
---
+++ Operations_operators/test_all_2.m
@@ -0,0 +1,7 @@
+A=[0 1 2;3 4 5]
+disp('all(A)') % 按照列的殊顺序
+all(A)
+disp('all(A,2)') % 按照行的顺序
+all(A,2)
+c=rand(1,2,3) 
+all(c)
